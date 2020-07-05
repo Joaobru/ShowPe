9efb2e334b98f50eb3e8fbb3e78d49231f3ec36e
joaobru <joaopaulobruno123@gmail.com>
2020-07-05 05:32:01 -0300

    Estilizações favoritos

--- src/pages/Evaluation/index.js
+++ src/pages/Evaluation/index.js
@@ -1,26 +1,31 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, Image } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import Box from '../../components/Box';
 import {Bar} from '../../assets/dados/Bar';
 import { Flat,Container } from './style';
+import Background from '../../components/Background/index';
 
 function Evaluation() {
   return(
-    <Container>
-    <Text>Favoritos:</Text>
+    <Background>
+      <Container>
+      <Text>Favoritos:</Text>
 
-    <Flat
-    data={Bar}
-    keyExtractor={(item) => String(item)}
-    renderItem={({ item }) => {
-      return(
-        <Box>
-          <Text>Hello</Text>
-        </Box>
-      );
-    }}/>
-    </Container>
+      <Flat
+      data={Bar}
+      keyExtractor={(item) => String(item)}
+      renderItem={({ item }) => {
+        return(
+          <Box>
+            <Text>{item.title}</Text>
+            <Text>{item.description}</Text>
+            <Image source={{ uri: item.url }}/>
+          </Box>
+        );
+      }}/>
+      </Container>
+    </Background>
   );
 }
 
